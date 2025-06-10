2e6a412d1978b5f4c5d772e95f706e4730025420
georgegyan <georgegyankwame72@mail.com>
2025-06-10 21:07:16 +0000

    Handle input validation Use serializers to register and authenticate Return required output formats for both endpoints

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import RegistrationSerializer, LoginSerializer
 
-# Create your views here.
+class RegistrationView(APIView):
+    def post(self, request):
+        serializer =  RegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({
+                "user_id": user.id,
+                "email": user.email,
+                "full_name": user.full_name
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            return Response(serializer.validated_data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_401_UNAUTHORIZED)
